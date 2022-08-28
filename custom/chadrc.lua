ec746b1c09889b4c313820aeda163a7772deed99
Aashish KC <kcaashish@gmail.com>
2022-08-28 14:04:21 +0545

    add setup for lspconfig

---
+++ custom/chadrc.lua
@@ -0,0 +1,24 @@
+-- Just an example, supposed to be placed in /lua/custom/
+
+local M = {}
+
+-- make sure you maintain the structure of `core/default_config.lua` here,
+-- example of changing theme:
+
+M.ui = {
+  theme = "ayu-dark",
+}
+
+M.plugins = {
+
+  user = {
+    ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+    },
+  }
+}
+
+return M
